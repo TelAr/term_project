f40d5750f7a3d631b87c58abbaf9fb77a175df56
TelAr <qet112313@gmail.com>
2020-12-05 20:09:44 +0900

    edit comment
    152 line part has wrong word

--- linked_list.c
+++ linked_list.c
@@ -149,7 +149,7 @@ static int search_back(void * _arg) {
 		
 
 		
-		if(thread_search_over){//if one thread is over, then this boolean flag change True and run br
+		if(thread_search_over){//if one thread is over, then this boolean flag change True and run break
 
 			break;
 		}
